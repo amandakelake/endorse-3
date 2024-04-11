7598a3b92a10aae40978a6d338697e570187e5fe
LGC <luoguangcong@corp.netease.com>
2024-04-11 14:10:42 +0800

    feat: install @ethsign/sp-sdk

---
+++ src/components/signProtocol.tsx
@@ -0,0 +1,14 @@
+// base sepolia test schema : https://testnet-scan.sign.global/schema/onchain_evm_84532_0x3f
+
+export interface IProps {
+
+}
+const SignProtocol  = (props: IProps) => {
+	return (
+		<div>
+			SignProtocol
+		</div>
+	)
+}
+
+export default SignProtocol
